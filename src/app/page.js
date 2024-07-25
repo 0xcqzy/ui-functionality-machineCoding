676a67c5c1d2338bf3fd345559d374518d7decf3
jayantibalmuchu <jayanti98b@gmail.com>
2024-07-26 01:28:32 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,24 @@
+export default function Home() {
+  return (
+    <main className="w-100 flex justify-evenly p-10" >
+      
+      <div className=" flex flex-col gap-3" >
+        <div className="h-10 w-10 border "></div>
+        <div className="h-10 w-10 border "></div>
+        <div className="h-10 w-10 border "></div>
+      </div>
+
+      <div className="h-100 flex flex-col justify-between gap-3">
+        <div className="h-10 w-10 border "></div>
+        <div className="h-10 w-10 border "></div>
+
+      </div>
+
+      <div className="h-100 flex flex-col justify-between gap-3">
+        <div className="h-10 w-10 border "></div>
+        <div className="h-10 w-10 border "></div>
+
+      </div>
+    </main>
+  );
+}
